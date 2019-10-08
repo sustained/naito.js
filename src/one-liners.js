09a3cce3e7d3a87af080bc74e03c5aceb0ca8180
Adam Dorling <adam.x.dorling@gmail.com>
2019-10-08 15:06:45 +0100

    :sparkles: some,every,map,foreach,filter,includes,slice,zip,intersection,union

---
+++ src/one-liners.js
@@ -0,0 +1,15 @@
+/**
+ * What? Did you think there was going to be a bunch of jokes in here... cos you're correct, all my code is a joke
+ */
+export const arraySome = (arr, fn) => arr.reduce((carry, item, index, array) => Boolean(fn(item, index, array)) || carry, false)
+export const arrayEvery = (arr, fn) => arr.reduce((carry, item, index, array) => Boolean(fn(item, index, array)) && carry, true)
+export const arrayMap = (arr, fn) => arr.reduce((carry, item, index, array) => carry.push(fn(item, index, array)) && carry, [])
+export const arrayForEach = (arr, fn) => arr.reduce((_, item, index, array) => void fn(item, index, array),null)
+export const arrayFilter = (arr, fn) => arr.reduce((carry, item, index, array) => fn(item, index, array) ? carry.push(item) || [] : carry, [])
+export const arrayIncludes = (arr,needle) => arr.reduce((carry,item) => item !== item ? carry || (needle!== needle): item === needle || item ,false)
+export const arraySlice = (arr, start, stop = Infinity) => (void (start = (arr.length+start) % arr.length)) || (void (stop = Math.abs(stop) === Infinity ? stop : (stop + arr.length) % arr.length )) || arr.reduce((carry,item,index) => (index >= start && index <= stop) ? (void carry.push(item)) || carry : carry,[])
+
+// Bonus
+export const arrayZip = (arrGroup) => Array.from({length: Math.max(...arrayMap(arrGroup, ({length}) => length))},(_,i)=>i).reduce((carry,index) => (void carry.push(arrGroup.reduce((acc,item) => (void acc.push(item[index])) || acc ,[])))|| carry,[])
+export const arrayIntersection = (arrGroup) => arrGroup.reduce((item,index) => index ? item : arrayFilter(item, item => arrayEvery(arraySlice(arrGroupitem,1), (internalArray) => arrayIncludes(internalArray, item) ),null))
+export const arrayUnion = (arrGroup) => [...arrGroup.reduce((carry,arr) => arrayForEach(arr, (item) => carry.add(item)) || carry, new Set())]
